7d2cc2c1440b8f47ae6c751f52995dacbc5d0e5d
Your Name <you@example.com>
2021-03-12 06:47:56 +0000

    adding project

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    html = "<h3> Prediction : From Google Pipelines (Continuous Delivery)</h3>"
+    return html.format(format)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=8080, debug=True)
