a01b3fa7ba4f07b2143a642be204101cb4f81fcf
james huddleston <james.huddleston@webwrapper.live>
2021-03-14 00:39:02 +0000

    push main

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ def home():
     return html.format(format)
 #!/usr/bin/env python
 
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('I update automatically!')
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8080, debug=True)
